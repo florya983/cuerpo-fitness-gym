3fddaa2da4fe218ebb1ba3fd7ebbf507d8d7d57f
Florencia yañez <florya0123@gmail.com>
2026-09-06 23:07:05 -0300

    Cambia mensajes de validación de formularios

--- script.js
+++ script.js
@@ -5,22 +5,27 @@ formularioAcceso.addEventListener("submit", function(event) {
 
     const dni = document.querySelector("#dni-acceso").value.trim();
     const password = document.querySelector("#password").value.trim();
+    const errorDni = document.querySelector("#error-dni-acceso");
+    const errorPassword = document.querySelector("#error-password");
+    errorDni.textContent = "";
+    errorPassword.textContent = "";
+
 
     if (dni === "") {
         event.preventDefault();
-        alert("Tenés que completar el DNI.");
+        errorDni.textContent = "Debes completar DNI";
         return;
     }
 
     if (isNaN(dni)) {
         event.preventDefault();
-        alert("El DNI debe contener solamente números.");
+        errorDni.textContent = "El DNI debe contener solamente números";
         return;
     }
 
     if (password === "") {
         event.preventDefault();
-        alert("Tenés que completar la contraseña.");
+        errorPassword.textContent = "Debes completar la contraseña";
         return;
     }
 
@@ -34,30 +39,39 @@ formularioRegistro.addEventListener("submit", function(event) {
     const nombre = document.querySelector("#apellido").value.trim();
     const dni = document.querySelector("#dni-registro").value.trim();
     const email = document.querySelector("#email").value.trim();
+    const errorNombre = document.querySelector("#error-nombre");
+    const errorDni = document.querySelector("#error-dni-registro");
+    const errorEmail = document.querySelector("#error-email");
+
+    errorNombre.textContent = "";
+    errorDni.textContent = "";
+    errorEmail.textContent = "";
 
     if (nombre === "") {
         event.preventDefault();
-        alert("Tenés que completar el nombre.");
+        errorNombre.textContent = "Debes completar el nombre";
         return;
     }
 
     if (dni === "") {
         event.preventDefault();
-        alert("Tenés que completar el DNI.");
+        errorDni.textContent = "Debes completar DNI";
         return;
     }
 
     if (isNaN(dni)) {
         event.preventDefault();
-        alert("El DNI debe contener solamente números.");
+        errorDni.textContent = "El DNI debe contener solamente números";
         return;
     }
 
     if (email === "") {
         event.preventDefault();
-        alert("Tenés que completar el email.");
+        errorEmail.textContent = "Debes completar el email";
         return;
     }
 
 });
+
+
 
